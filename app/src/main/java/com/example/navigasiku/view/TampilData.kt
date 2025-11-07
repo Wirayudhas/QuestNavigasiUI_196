e110d3191e218c747e8bc10709eabd21db47e031
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-07 17:07:04 +0700

    memberikan jarak dan menambahkan tombol dengan ukuran fillmaxwidth berisi teks "back"

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -71,4 +71,14 @@ fun TampilData(
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
             }
+            Spacer(modifier = Modifier.height(10.dp))
 
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.back))
+            }
+        }
+    }
+}
