192a537a312f5e433af2adedae95c69733892ef4
[Arthur Gerald] <[arthur.gerald.ft23@mail.umy.ac.id]>
2025-11-07 17:52:04 +0700

    membuat fungsi yang dapat menavigasi halaman dengan NavController dan NavHost dan membuat logika untuk kembali ke halaman sebellumnya

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
